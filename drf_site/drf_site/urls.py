819ca800704438a7551060e7a81d4755ee5aefac
Кулагин Андрей Андреевич <s121917@guu.ru>
2023-04-17 12:24:32 +0300

    final

--- drf_site/drf_site/urls.py
+++ drf_site/drf_site/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
 from women.views import *
 from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'women', WomenViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/women_list', WomenViewSet.as_view({'get': 'list'})),
-    path('api/v1/women_list/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
+    path('api/v1/', include(router.urls)),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
+    path('api/v1/auth', include('djoser.urls')),
+    re_path(r'^auth/', include("djoser.urls.authtoken")),
+    # path('api/v1/women_list', WomenViewSet.as_view({'get': 'list'})),
+    # path('api/v1/women_list/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
 
 ]
--- drf_site/women/views.py
+++ drf_site/women/views.py
@@ -1,5 +1,6 @@
 from django.forms import model_to_dict
 from django.shortcuts import render
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Women
@@ -12,3 +13,5 @@ from rest_framework import generics, viewsets
 class WomenViewSet(viewsets.ModelViewSet):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
+
+
